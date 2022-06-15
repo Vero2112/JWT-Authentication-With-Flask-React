5840190f673103d0b61f1030919b62f42d95cf0b
Vero2112 <verorodriguezruiz21@gmail.com>
2022-06-15 19:29:10 +0000

    change home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,19 +8,25 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
+			
+			<div class="mb-3 row">
+    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+    <div class="col-sm-10">
+      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com"></input>
+    </div>
+  </div>
+  <div class="mb-3 row">
+    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword" ></input>
+    </div>
+	<div> 
+    <button type="submit" class="btn btn-primary mb-3 mt-2">Confirm identity</button>
+ 	</div>
+  </div>
+				
+
+
 		</div>
 	);
 };
